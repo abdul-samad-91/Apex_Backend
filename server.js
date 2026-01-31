260c264ee66261989335f81ad0ea6276405783cb
Hanzala Khattak <muhammadhanzalahkhan1@gmail.com>
2026-02-01 00:16:24 +0500

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
     const express = require('express');
     require('dotenv').config();
+    const cors = require('cors');
     const connectDB = require('./Config/DB');
     const userRoutes = require('./Routes/user.routes');
     const transactionRoutes = require('./Routes/transaction.routes');
@@ -12,6 +13,14 @@
 
     const app = express();
     const PORT = process.env.PORT || 3000;
+
+    // CORS configuration
+    app.use(cors({
+        origin: ['http://localhost:5173', 'http://localhost:3000'],
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }));
 
     // Middleware
     app.use(express.json());
