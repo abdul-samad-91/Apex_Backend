3f4d1ee9db440c6c229909f8868243c65b326d1a
Abdul Samad <as03489822@gmail.com>
2026-02-02 16:23:59 +0500

    update cors

--- server.js
+++ server.js
@@ -29,7 +29,8 @@
     // Middleware
     
     app.use(cors({
-        origin: ['http://localhost:5173','https://apex-admin-gules.vercel.app'],
+        // origin: ['http://localhost:5173','https://apex-admin-gules.vercel.app'],
+        origin: 'https://apex-admin-gules.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         // allowedHeaders: ['Content-Type', 'Authorization']
     }));
