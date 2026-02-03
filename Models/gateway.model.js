dc43d193e13c43eda20ecfde33bc473f1b108393
Hanzala Khattak <muhammadhanzalahkhan1@gmail.com>
2026-02-03 17:13:48 +0500

    updated the gatewaymodel

--- Models/gateway.model.js
+++ Models/gateway.model.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const gatewaySchema = new mongoose.Schema(
   {
     bankImageUrl: { type: String },
-    bankName: { type: String, required: true, trim: true },
-    bankAccountNumber: { type: String, required: true, trim: true },
+    bankName: { type: String, trim: true },
+    bankAccountNumber: { type: String, trim: true },
     bankRoutingNumber: { type: String, trim: true },
     bankBranchName: { type: String, trim: true },
-    gatewayCurrency: { type: String, required: true, trim: true },
+    gatewayCurrency: { type: String, trim: true },
     conversionRate: { type: Number, default: 1 },
     charge: { type: Number, default: 0 },
     allowAsPaymentMethod: { type: Boolean, default: false },
